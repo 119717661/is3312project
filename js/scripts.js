499cd80521dca7b7978359a0b85fac055681a611
119717661 <62059603+119717661@users.noreply.github.com>
2021-10-20 18:36:07 +0100

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,14 @@
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
 */
 // Java Script
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+  document.body.style.backgroundColor = "white";
+}
